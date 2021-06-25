be6508cf5f02d1629c639910531f6427b782afec
EricWDuarte <38636378+EricWDuarte@users.noreply.github.com>
2021-06-25 13:56:54 -0300

    Add files via upload

---
+++ Project Euler/Exercise 20/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_20
+{
+    class Program
+    {
+        static void Main()
+        {
+            BigInteger n = 1;
+            int result = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                n *= i;
+            }
+
+            string s = n.ToString();
+
+            while(s.Length > 0)
+            {
+                result += int.Parse(s.Substring(0, 1));
+                s = s.Remove(0, 1);
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
