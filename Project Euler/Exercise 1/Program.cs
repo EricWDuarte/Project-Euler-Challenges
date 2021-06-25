c2b501e45c4c860e6b5b06b1c7138e67e140d447
EricWDuarte <38636378+EricWDuarte@users.noreply.github.com>
2021-06-25 14:14:42 -0300

    Add files via upload

---
+++ Project Euler/Exercise 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+public class Program
+{
+    static void Main()
+    {
+        float n = 0;
+        int result = 0;
+
+        ArrayList multiples3n5 = new ArrayList();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            n = i;
+            if (n % 15 == 0)
+            {
+                multiples3n5.Add(i);
+            }
+            else if (n % 5 == 0)
+            {
+                multiples3n5.Add(i);
+            }
+            else if (n % 3 == 0)
+            {
+                multiples3n5.Add(i);
+            }
+        }
+
+        foreach (int number in multiples3n5)
+        {
+            Console.WriteLine(number);
+            result += number;
+        }
+
+        Console.WriteLine(result);
+    }
+}
