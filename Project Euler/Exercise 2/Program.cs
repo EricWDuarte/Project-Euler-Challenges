962e03ab258106231ae36d723360059fe95aac30
EricWDuarte <38636378+EricWDuarte@users.noreply.github.com>
2021-06-25 12:56:40 -0300

    Add files via upload

---
+++ Project Euler/Exercise 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+public class Program
+{
+	static void Main()
+	{
+        ArrayList fibonacci = new ArrayList();
+
+        fibonacci.Add(1);
+        fibonacci.Add(1);
+
+        int i = 1;
+
+        do
+        {
+            i++;
+            int sum = (int)fibonacci[i - 1] + (int)fibonacci[i - 2];
+            fibonacci.Add(sum);
+            Console.WriteLine(sum);
+        }
+        while ((int)fibonacci[i] < 4000000);
+
+        Console.WriteLine("");
+        int result = 0;
+
+
+        for(int o = 2; o < fibonacci.Count; o += 3)
+        {
+            result += (int)fibonacci[o];
+        }
+
+        Console.WriteLine("Result = {0}", result);
+
+	}
+}
