8844ebaeff434eafd4a566dfe66c8f3104fecf59
EricWDuarte <38636378+EricWDuarte@users.noreply.github.com>
2021-06-25 14:03:30 -0300

    Add files via upload

---
+++ Project Euler/Exercise 25/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_25
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string numberS = "";
+            BigInteger a = 1;
+            BigInteger b = 1;
+            BigInteger t = 0;
+            int n = 2;
+
+            while (numberS.Length < 1000)
+            {
+                a = a + b;
+                numberS = a.ToString();
+
+                t = a;
+                a = b;
+                b = t;
+                n++;
+
+                Console.WriteLine(a)
+            }
+
+            Console.WriteLine("The first 1000 digit Fibonacci is in the {0}th position", n);
+            Console.ReadLine();
+
+        }
+    }
+}
